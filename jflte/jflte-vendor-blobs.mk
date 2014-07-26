4d86ef639165d42762aaeef6deb64ba9e36dbdc9
Dan Pasanen <dan.pasanen@gmail.com>
2014-07-26 09:42:19 -0500

    jf: update adreno
    6/24/14 from qdevnet
    
    Change-Id: I9af3f991993108935b34a39f207101fad0c1805a

--- jflte/jflte-vendor-blobs.mk
+++ jflte/jflte-vendor-blobs.mk
@@ -41,6 +41,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/jflte/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+	vendor/samsung/jflte/proprietary/vendor/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
@@ -52,6 +53,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/jflte/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
+	vendor/samsung/jflte/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
 	vendor/samsung/jflte/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
